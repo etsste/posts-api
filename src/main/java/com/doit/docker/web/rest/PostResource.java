1d007dcd517e943ae43f2be20060515f358e975f
Sinan Sümertaş <sinan.sumertas@etstur.com>
2019-03-26 18:53:18 +0300

    log eklendi

--- src/main/java/com/doit/docker/web/rest/PostResource.java
+++ src/main/java/com/doit/docker/web/rest/PostResource.java
@@ -2,6 +2,9 @@ package com.doit.docker.web.rest;
 
 import com.doit.docker.domain.Post;
 import com.doit.docker.repository.PostRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.json.GsonJsonParser;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class PostResource {
 
+    private final Logger log = LoggerFactory.getLogger(PostResource.class);
+
 
     private PostRepository postRepository;
 
@@ -26,28 +31,32 @@ public class PostResource {
 
     @PostMapping("/posts")
     public Post createPost(@RequestBody Post post) {
+        log.info("createPost");
         return postRepository.save(post);
     }
 
     @GetMapping("/posts")
     public Page<Post> getAllPosts(Pageable pageable) {
+        log.info("getAllPosts");
         return postRepository.findAll(pageable);
     }
 
     @GetMapping("/posts/{id}")
     public Post getPost(@PathVariable Long id) {
+        log.info("getPost");
         return postRepository.findById(id).orElseThrow(() -> new RuntimeException("Yok boyle bi sey"));
     }
 
     @PutMapping("/posts")
     public Post updatePost(@RequestBody Post post) {
+        log.info("updatePost");
         return postRepository.save(post);
     }
 
     @DeleteMapping("/posts/{id}")
     public void deletePost(@PathVariable Long id) {
+        log.info("deletePost");
         postRepository.deleteById(id);
     }
-
 
 }
